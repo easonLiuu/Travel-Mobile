9b05d7ce88d816330701b4c75efb0b4f86ace8ab
liujiarui <ljr3289595084@163.com>
2023-03-02 11:27:17 +0800

    feat: add swiper

--- src/main.js
+++ src/main.js
@@ -4,11 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
+Vue.use(VueAwesomeSwiper)
+
 fastClick.attach(document.body)
 
 /* eslint-disable no-new */
