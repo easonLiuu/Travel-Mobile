98f55b1b76cc0872ead921f0519544b9c9ebfa86
liujiarui <ljr3289595084@163.com>
2023-03-03 11:31:03 +0800

    feat: use vuex pages linkage

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -18,6 +18,7 @@
                     class="search-item border-bottom"
                     v-for="item of list"
                     :key="item.id"
+                    @click="handleCityClick(item.name)"
                 >{{item.name}}</li>
                 <li class="search-item border-bottom" v-show="hasNoData">
                     没有找到匹配数据!
@@ -29,6 +30,7 @@
 
 <script>
 import Bscroll from 'better-scroll'
+import { mapMutations } from 'vuex'
 export default {
   name: 'CitySearch',
   props: {
@@ -67,6 +69,13 @@ export default {
         this.list = result
       }, 100)
     }
+  },
+  methods: {
+    handleCityClick (city) {
+      this.changeCity(city)
+      this.$router.push('/')
+    },
+    ...mapMutations(['changeCity'])
   },
   mounted () {
     this.scroll = new Bscroll(this.$refs.search)
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -9,7 +9,7 @@
         </div>
         <router-link to='/city'>
             <div class="header-right">
-                {{this.city}}
+                {{this.doubleCity}}
                 <span class="iconfont arrow-icon">&#xe64a;</span>
             </div>
         </router-link>
@@ -17,10 +17,12 @@
 </template>
 
 <script>
+import { mapGetters, mapState } from 'vuex'
 export default {
   name: 'HomeHeader',
-  props: {
-    city: String
+  computed: {
+    ...mapState(['city']),
+    ...mapGetters(['doubleCity'])
   }
 }
 </script>
@@ -49,7 +51,8 @@ export default {
       border-radius: .1rem
       color: #ccc
     .header-right
-      width: 1.24rem
+      min-width: 1.04rem
+      padding: 0 .1rem
       float: right
       text-align: center
       color: #fff
