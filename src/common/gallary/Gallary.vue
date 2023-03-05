79ffa7d547589598873513694d9fe2c57abd4e88
liujiarui <ljr3289595084@163.com>
2023-03-05 11:02:51 +0800

    banner finished

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="container" @click="handleGallaryClick">
+        <div class="wrapper">
+            <swiper :options="swiperOptions">
+                <swiper-slide v-for="(item, index) in imgs" :key="index">
+                    <img class="gallary-img" :src="item" />
+                </swiper-slide>
+                <div class="swiper-pagination"  slot="pagination"></div>
+            </swiper>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CommonGallary',
+  props: {
+    imgs: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+      swiperOptions: {
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction',
+        // 解决swiper宽度计算的问题 当DOM结构发生变化时 重新计算宽度
+        observeParents: true,
+        observer: true
+      }
+    }
+  },
+  methods: {
+    handleGallaryClick () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.container >>> .swiper-container
+    overflow: inherit
+.container
+    display: flex
+    flex-direction: column
+    justify-content: center
+    z-index: 99
+    position: fixed
+    left: 0
+    right: 0
+    top: 0
+    bottom: 0
+    background: #000
+    .wrapper
+        width: 100%
+        height: 0
+        padding-bottom: 100%
+        .gallary-img
+            width: 100%
+        .swiper-pagination
+            color: #fff
+            bottom: -1rem
+</style>
